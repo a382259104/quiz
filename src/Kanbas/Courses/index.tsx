ce57b0d73cbbf7ab406996a2144bea87b75ae897
YuFeng Lin <a382259104@gmail.com>
2024-02-23 12:34:11 -0500

    gave up lol

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import CourseNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
+import Assignments from "./Assignments";
 
 function Courses() {
 
@@ -34,7 +35,7 @@ function Courses() {
                             <Route path="Home" element={<Home/>} />
                             <Route path="Modules" element={<Modules />} />
                             <Route path="Piazza" element={<h1>Piazza</h1>} />
-                            <Route path="Assignments" element={<h1>Assignments</h1>} />
+                            <Route path="Assignments" element={<Assignments/>} />
                             <Route path="Assignments/:assignmentId" element={<h1>Assignment Editor</h1>} />
                             <Route path="Grades" element={<h1>Grades</h1>} />
                         </Routes>
